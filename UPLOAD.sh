afada120f0908fffe04f489f443c2507d6609b53
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2019-05-07 16:27:36 +0200

    added deploy script

---
+++ UPLOAD.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#rm -rf _site/*
+#mkdir -p _site
+#tar czf _site/jekyll-source.tgz _layouts 2* assets .gitignore .htaccess *.yml *.json *.md
+#jekyll build
+#scp -r ./_site/* ./_site/.htaccess zsolt@storm:/var/www/deepdata.hu/koko/
+scp -r css data js index.html zsolt@storm:/var/www/deepdata.hu/koko/
