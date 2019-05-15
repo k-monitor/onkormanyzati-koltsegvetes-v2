fa66e452b1ff9be7a3a75e6aeac3a8b87864b509
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2019-05-15 23:55:42 +0200

    generating economic category tree

--- scripts/prepare-data.js
+++ scripts/prepare-data.js
@@ -10,17 +10,25 @@ const INPUT_FILE = 'data/src/tápió adatok.xlsx';
 // main script
 
 (() => {
+	console.log(`Processing file: ${INPUT_FILE}`);
 	const workbook = xlsx.readFile(INPUT_FILE);
+
 	workbook.SheetNames.forEach(sheet => {
+		console.log(`Processing sheet: ${sheet}`);
+		const sheetTsv = xlsx.utils.sheet_to_csv(workbook.Sheets[sheet], { FS: '\t' });
+
 		const year = sheet.split(' ')[0];
 		const name = sheet.split(' ')[1]
 			.replace('BEVÉTEL', 'income')
 			.replace('KIADÁS', 'expense');
-		const matrixFile = `data/${year}/${name}.tsv`;
 
-		console.log(`Converting ${INPUT_FILE}[${sheet}] -> ${matrixFile}`);
-		const sheetTsv = xlsx.utils.sheet_to_csv(workbook.Sheets[sheet], { FS: '\t' });
+		const matrixFile = `data/${year}/${name}.tsv`;
+		console.log(`\tGenerating matrix -> ${matrixFile}`);
 		convertBudget(sheetTsv, matrixFile);
+
+		const treeFile = `data/${year}/${name}-tree.tsv`;
+		console.log(`\tGenerating tree -> ${treeFile}`);
+		generateTree(sheetTsv, treeFile);
 	});
 })();
 
@@ -37,21 +45,78 @@ function convertBudget(tsv, outputFile) {
 		if (i == 0) {
 			funcIds = cols.map(s => s.split(' ')[0]);
 			funcIds[2] = '*';
-		} else if (cols[0].trim().match(/\d{2}/)) { // valid econ ID
+		} else if (cols[0].trim().match(/^\d{2,}$/)) { // valid econ ID
 			const econId = Number(cols[0]);
 			const econDesc = cols[1];
 			if (econDesc.indexOf('=') == -1) {
 				for (let j = 2; j < cols.length; j++) {
-					const outputLine = [
-						econId,
-						funcIds[j],
-						cols[j].replace(/\D+/g, '')
-					].join('\t');
-					outputLines.push(outputLine);
+					const value = Number(cols[j].replace(/\D+/g, ''));
+					if (value > 0) {
+						const outputLine = [
+							econId,
+							funcIds[j],
+							value
+						].join('\t');
+						outputLines.push(outputLine);
+					}
 				}
 			}
 		}
 	});
 
 	fs.writeFileSync(outputFile, outputLines.join('\n'));
+}
+
+function generateTree(tsv, outputFile) {
+	const rows = tsv.split('\n').splice(2); // remove first 2 rows
+	const nodes = {};
+	rows.forEach(row => {
+		const cols = row.split('\t');
+		if (cols[0].trim().match(/^\d{2,}$/)) { // valid econ ID
+			const id = Number(cols[0]);
+			const name = cols[1]
+				.replace(/ \([BK0-9\-]+\)/g, '') // remove alt. ID
+				.replace(/ \([=>0-9+….]+\)/, '') // remove equation
+				.trim();
+			const formulaMatch = cols[1].match(/ \(\D*([0-9+….]+)\)/);
+			const formula = formulaMatch ? formulaMatch[1] : '';
+			const children = parseFormula(formula);
+			nodes[Number(id)] = {
+				id,
+				name,
+				children
+			};
+		}
+	});
+	Object.values(nodes).forEach(parent => {
+		parent.children.forEach(childId => {
+			if (nodes[childId]) {
+				nodes[childId].parent = parent.id;
+			}
+		});
+	});
+
+	const outputLines = [];
+	Object.values(nodes).forEach(node => {
+		const outputLine = [
+			node.id,
+			node.name,
+			node.parent || 0
+		].join('\t');
+		outputLines.push(outputLine);
+	});
+	fs.writeFileSync(outputFile, outputLines.join('\n'));
+}
+
+function parseFormula(f) {
+	const ids = [];
+	f.replace(/\+[….]+\+/, ':').split('\+').forEach(el => {
+		if (el.match(/^\d+$/g)) {
+			ids.push(Number(el));
+		} else if (el.indexOf(':') > -1) {
+			const bounds = el.split(':').map(b => b.match(/^\d+$/) ? Number(b) : 0);
+			for (let i = bounds[0]; i <= bounds[1]; i++) ids.push(i);
+		}
+	});
+	return ids;
 }
