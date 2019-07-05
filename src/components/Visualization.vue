2d533e29d29f055268e8b07fb09f3d1325674336
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2019-07-05 10:07:14 +0200

    added vis mode switcher + breadcrumbs

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -1,5 +1,54 @@
 <template>
+	<div class="visualization">
+		<div class="row justify-content-center">
+			<div
+			 class="col-lg-8 text-center"
+			 v-if="data.econ && data.func"
+			>
+				<ul class="justify-content-center mb-5 nav nav-pills w-100">
+					<li class="nav-item">
+						<a
+						 class="nav-link"
+						 href="javascript:void(0)"
+						 data-toggle="tooltip"
+						 data-placement="bottom"
+						 title="Működési vagy felhalmozási jellegük alapján mutatja meg a kiadások összetételét, hogy mekkora a személyi kiadások, a kapcsolódó munkáltatói járulékok, a dologi kiadások, a beruházási és felújítási kiadások, az államháztartáson belüli és kívüli támogatások, transzferek összege a költségvetésben."
+						 :class="{ active: mode == 0 }"
+						 @click="path = []; mode = 0"
+						>
+							Közgazdasági nézet
+						</a>
+					</li>
+					<li class="nav-item">
+						<a
+						 class="nav-link"
+						 href="javascript:void(0)"
+						 data-toggle="tooltip"
+						 data-placement="bottom"
+						 title="A költségvetési kiadásokat osztályozza, azok társadalmi-gazdasági cél szerinti összetételét mutatja. Az általános közszolgáltatásoktól a védelmi kiadásokig összesen 10 kategóriában tartalmazza a kerület működésének területeit."
+						 :class="{ active: mode == 1 }"
+						 @click="path = []; mode = 1"
+						>
+							Funkcionális nézet
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 
+		<nav aria-label="breadcrumb">
+			<ol class="breadcrumb">
+				<li
+				 class="breadcrumb-item"
+				 v-for="(n,i) in nodePath"
+				 :key="i"
+				 :class="{ active: i == nodePath.length - 1 }"
+				 @click="up(path.length - i)"
+				>{{ n.name }}</li>
+				<div class="ml-auto subtotal">{{ $util.groupNums(node.value) }}</div>
+			</ol>
+		</nav>
+	</div>
 </template>
 
 <script>
@@ -108,6 +157,39 @@ export default {
 				});
 			return ids.indexOf(id);
 		},
+		curve(id, node, index) {
+			try {
+				var bars = $("#" + id);
+				var barsTop = $(bars).offset().top;
+
+				var bar = $("#" + id + " .bar[data-index=" + index + "]");
+				var barHeight = $(bar).outerHeight();
+				var barTop = $(bar).offset().top - barsTop;
+				var barMiddle = barTop + barHeight / 2;
+
+				var label = $("#" + id + " .label[data-index=" + index + "]");
+				var labelHeight = $(label).outerHeight();
+				var labelTop = $(label).offset().top - barsTop;
+				var labelMiddle = labelTop + labelHeight / 2;
+
+				var svg = $("#" + id + " .curves svg");
+				var svgWidth = $(svg).outerWidth();
+
+				var x1 = 0;
+				var y1 = barMiddle;
+				var x2 = svgWidth;
+				var y2 = labelMiddle; //self.labelY(node, index).slice(0, -1);
+				var cx1 = svgWidth * 0.2;
+				var cx2 = svgWidth * 0.8;
+				var m = x1 + "," + y1;
+				var c1 = cx1 + "," + y1;
+				var c2 = cx2 + "," + y2;
+				var e = x2 + "," + y2;
+				return ["M" + m, "C" + c1, c2, e].join(" ");
+			} catch (e) {
+				return "";
+			}
+		},
 		down: function(index) {
 			$(".tooltip").remove();
 			if (
@@ -132,7 +214,7 @@ export default {
 
 			var self = this;
 			this.curves = this.children.map(function(n, i) {
-				return curve(self.id, n, i);
+				return self.curve(self.id, n, i);
 			});
 		},
 		regenerateTooltips() {
@@ -151,3 +233,85 @@ export default {
 	}
 };
 </script>
+
+<style lang="scss">
+@import "../scss/variables";
+@import "~bootstrap/scss/functions";
+@import "~bootstrap/scss/variables";
+@import "~bootstrap/scss/mixins";
+
+.visualization {
+	.left-column,
+	.right-column {
+		width: 45%;
+	}
+
+	.middle-column {
+		width: 10%;
+	}
+
+	.bar,
+	.label {
+		align-items: center;
+		display: flex;
+		flex: 1;
+		margin-bottom: 1px;
+		padding: 0.1rem 0.5rem;
+	}
+
+	.back-bar {
+		cursor: pointer;
+		margin-right: 1px;
+
+		&:hover {
+			opacity: 0.8;
+		}
+	}
+
+	.breadcrumb-item.active,
+	.breadcrumb .subtotal {
+		color: $dark;
+		font-weight: bold;
+	}
+
+	.breadcrumb-item:not(.active) {
+		&:not(:hover) {
+			color: $text-muted;
+		}
+		cursor: pointer;
+	}
+
+	.curve {
+		fill: none;
+	}
+
+	.label {
+		text-align: left;
+	}
+
+	svg {
+		height: 100%;
+		width: 100%;
+	}
+
+	.vis-header {
+		font-weight: bold;
+	}
+
+	.vis {
+		height: 50vh;
+		font-size: 90%;
+		min-height: 400px;
+
+		& > div {
+			height: 100%;
+		}
+
+		.bar,
+		.label {
+			cursor: pointer;
+		}
+	}
+}
+</style>
+
