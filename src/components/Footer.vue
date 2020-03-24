17656d049fc2b1827b2011914a35e74c509d8d0c
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2020-03-24 16:24:32 +0100

    added GPL-3.0 license

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,65 +3,70 @@
 		<div class="container">
 			<div class="row">
 				<div
-				 class="col-sm-6 col-lg-4"
-				 id="city-logo"
+					class="col-sm-6 col-lg-4"
+					id="city-logo"
 				>
 					<a
-					 class="js-scroll-trigger"
-					 href="#page-top"
+						class="js-scroll-trigger"
+						href="#page-top"
 					>
 						<img
-						 src="assets/img/logo.png"
-						 alt=""
+							src="assets/img/logo.png"
+							alt=""
 						>
 					</a>
 				</div>
 				<div
-				 class="col-sm-6 col-lg-4"
-				 id="logos"
+					class="col-sm-6 col-lg-4"
+					id="logos"
 				>
 					<a
-					 href="http://www.kfib.hu/"
-					 target="_blank"
+						href="http://www.kfib.hu/"
+						target="_blank"
 					><img
-						 src="assets/img/kfib-logo.png"
-						 alt=""
+							src="assets/img/kfib-logo.png"
+							alt=""
 						></a>
 					<a
-					 href="https://k-monitor.hu/"
-					 target="_blank"
+						href="https://k-monitor.hu/"
+						target="_blank"
 					><img
-						 src="assets/img/k-monitor-logo.png"
-						 alt=""
+							src="assets/img/k-monitor-logo.png"
+							alt=""
 						></a>
 					<a
-					 href="https://www.deepdata.hu/"
-					 target="_blank"
+						href="https://www.deepdata.hu/"
+						target="_blank"
 					><img
-						 src="assets/img/deepdata-logo.png"
-						 alt=""
+							src="assets/img/deepdata-logo.png"
+							alt=""
 						></a>
 				</div>
 				<div
-				 class="col-lg-4 mt-5 mt-lg-0"
-				 id="license"
+					class="col-lg-4 mt-5 mt-lg-0"
+					id="license"
 				>
 					<i class="fab fa-fw fa-creative-commons"></i>
 					<i class="fab fa-fw fa-creative-commons-by"></i>
 					<i class="fab fa-fw fa-creative-commons-sa mr-2"></i>
 					<br class="d-none d-lg-inline-block">
 					Ez a Mű a <a
-					 rel="license"
-					 href="http://creativecommons.org/licenses/by-sa/4.0/"
+						rel="license"
+						href="http://creativecommons.org/licenses/by-sa/4.0/"
 					>Creative Commons
 						Nevezd
 						meg! - Így add tovább! 4.0 Nemzetközi Licenc</a> feltételeinek megfelelően felhasználható.
 					<br>
 					<i class="fab fa-github fa-fw mr-2"></i>
 					<a
-					 href="https://github.com/k-monitor/onkormanyzati-koltsegvetes-v2"
-					 target="_blank"
+						href="https://github.com/k-monitor/onkormanyzati-koltsegvetes-v2"
+						target="_blank"
 					>Forráskód a GitHub-on</a>
+					(licensz:
+					<a
+						href="https://spdx.org/licenses/GPL-3.0.html"
+						target="_blank"
+					>GPL 3.0</a>)
 				</div>
 			</div>
 		</div>
