2658908225bfd234fea1428d1d697f4698e10409
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2020-11-04 13:44:37 +0100

    refactored SectionHeading to handle string years

--- src/components/SectionHeading.vue
+++ src/components/SectionHeading.vue
@@ -4,14 +4,14 @@
 		<small class="ml-3 text-muted">
 			<i
 				class="fas fa-caret-left"
-				v-if="$d[year - 1]"
-				@click="$eventBus.$emit('yearSelected', year - 1);"
+				v-if="$d[years[index - 1]]"
+				@click="$eventBus.$emit('yearSelected', years[index - 1])"
 			></i>
 			{{ year }}
 			<i
 				class="fas fa-caret-right"
-				v-if="$d[year + 1]"
-				@click="$eventBus.$emit('yearSelected', year + 1)"
+				v-if="$d[years[index + 1]]"
+				@click="$eventBus.$emit('yearSelected', years[index + 1])"
 			></i>
 		</small>
 	</h2>
@@ -19,6 +19,14 @@
 
 <script>
 export default {
-	props: ["title", "year"]
+	props: ["title", "year"],
+	computed: {
+		index() {
+			return this.years.indexOf(this.year);
+		},
+		years() {
+			return Object.keys(this.$d).sort();
+		}
+	}
 };
 </script>
