dd0a58211e8d508bb321b405daedd2dcf8d8b778
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2020-11-05 12:49:07 +0100

    refactored tooltips preparator to handle years

--- scripts/prepare-tooltips.js
+++ scripts/prepare-tooltips.js
@@ -5,16 +5,24 @@ const INPUT_FILE = './input/config.xlsx';
 const OUTPUT_FILE = './src/data/tooltips.json';
 
 const workbook = xlsx.readFile(INPUT_FILE);
-const tsv = xlsx.utils.sheet_to_csv(workbook.Sheets['tooltips'], { FS: '\t' });
 
 const output = {};
 
-tsv.split('\n').forEach((row, i) => {
-	if (i === 0) return;
-	let [id, _, tooltip] = row.split('\t');
-	if (id && tooltip) {
-		output[id] = tooltip.trim();
-	}
+workbook.SheetNames.filter(name => name.startsWith('tooltips ')).forEach(tooltipSheetName => {
+	const p = tooltipSheetName.split(' ');
+	p.shift();
+	const year = p.join(' ');
+
+	const tsv = xlsx.utils.sheet_to_csv(workbook.Sheets[tooltipSheetName], { FS: '\t' });
+
+	tsv.split('\n').forEach((row, i) => {
+		if (i === 0) return;
+		let [id, _, tooltip] = row.split('\t');
+		if (id && tooltip) {
+			output[year] = output[year] || {};
+			output[year][id] = tooltip.trim();
+		}
+	});
 });
 
 fs.writeFileSync(OUTPUT_FILE, JSON.stringify(output));
