2c8b4accf316bbb7705aad2ae250bb342c264403
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-09 16:58:05 +0100

    reworked footer, added contacts

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,62 +1,71 @@
 <template>
 	<footer class="py-5">
 		<div class="container">
-			<div class="row">
+			<div class="row mb-4">
 				<div
-					class="col-sm-6 col-lg-4"
+					class="col-12 col-lg-6 col-xl-7 d-flex"
 					id="city-logo"
 				>
-					<a href="https://www.terezvaros.hu/">
-						<img
-							src="assets/img/logo-big.png"
-							alt=""
-						>
-					</a>
+					<div class="mr-5">
+						<a href="https://www.terezvaros.hu/">
+							<img
+								src="assets/img/logo-big.png"
+								alt=""
+							>
+						</a>
+					</div>
+					<div class="ml-4 mt-3">
+						<p class="lead"><a
+								href="https://www.terezvaros.hu/"
+								target="_blank"
+							><i class="fas fa-fw fa-globe-europe mr-2"></i>terezvaros.hu</a></p>
+						<p class="lead"><a
+								href="https://www.facebook.com/terezvaros.hu"
+								target="_blank"
+							><i class="fab fa-fw fa-facebook mr-2"></i>facebook.com/terezvaros.hu</a></p>
+						<p class="lead"><a href="mailto:sajto@terezvaros.hu"><i class="fas fa-fw fa-envelope mr-2"></i>sajto@terezvaros.hu</a></p>
+					</div>
 				</div>
-				<div
-					class="col-sm-6 col-lg-4"
-					id="logos"
-				>
-					<a
-						href="https://k-monitor.hu/"
-						target="_blank"
-					><img
-							src="assets/img/k-monitor-logo.png"
-							alt=""
-						></a>
-					<a
-						href="https://www.deepdata.hu/"
-						target="_blank"
-					><img
-							src="assets/img/deepdata-logo.png"
-							alt=""
-						></a>
-				</div>
-				<div
-					class="col-lg-4 mt-5 mt-lg-0"
-					id="license"
-				>
-					<i class="fab fa-fw fa-creative-commons"></i>
-					<i class="fab fa-fw fa-creative-commons-by"></i>
-					<i class="fab fa-fw fa-creative-commons-sa mr-2"></i>
-					<br class="d-none d-lg-inline-block">
-					Ez a Mű a <a
-						rel="license"
-						href="http://creativecommons.org/licenses/by-sa/4.0/"
-					>Creative Commons
-						Nevezd
-						meg! - Így add tovább! 4.0 Nemzetközi Licenc</a> feltételeinek megfelelően felhasználható.
-					<br>
-					<i class="fab fa-github fa-fw mr-2"></i>
-					<a
-						href="https://github.com/k-monitor/onkormanyzati-koltsegvetes-v2"
-						target="_blank"
-					>Forráskód a GitHub-on</a>
-					(licensz:
-					<a
-						href="https://spdx.org/licenses/GPL-3.0.html"
-						target="_blank"
-					>GPL 3.0</a>)
+				<div class="col mt-5 mt-lg-0">
+					<div class="mb-3" id="logos">
+						<a
+							href="https://k-monitor.hu/"
+							target="_blank"
+						><img
+								src="assets/img/k-monitor-logo.png"
+								alt=""
+							></a>
+						<a
+							href="https://www.deepdata.hu/"
+							target="_blank"
+						><img
+								src="assets/img/deepdata-logo.png"
+								alt=""
+							></a>
+					</div>
+					<div id="license">
+						<i class="fab fa-fw fa-creative-commons"></i>
+						<i class="fab fa-fw fa-creative-commons-by"></i>
+						<i class="fab fa-fw fa-creative-commons-sa mr-2"></i>
+						<br class="d-none d-lg-inline-block">
+						Ez a Mű a <a
+							rel="license"
+							href="http://creativecommons.org/licenses/by-sa/4.0/"
+						>Creative Commons
+							Nevezd
+							meg! - Így add tovább! 4.0 Nemzetközi Licenc</a> feltételeinek megfelelően felhasználható.
+						<br>
+						<i class="fab fa-github fa-fw mr-2"></i>
+						<a
+							href="https://github.com/k-monitor/onkormanyzati-koltsegvetes-v2"
+							target="_blank"
+						>Forráskód a GitHub-on</a>
+						(licensz:
+						<a
+							href="https://spdx.org/licenses/GPL-3.0.html"
+							target="_blank"
+						>GPL 3.0</a>)
+					</div>
 				</div>
 			</div>
 		</div>
@@ -65,7 +74,7 @@
 
 <style lang="scss">
 #logos a {
-	opacity: .75;
+	opacity: 0.75;
 	&:hover {
 		opacity: 1;
 	}
