4cb2509c418e89ee6b0982607eee4b70bdf03be4
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-04-13 16:11:50 +0200

    added decimal to integers too in short form

--- src/main.js
+++ src/main.js
@@ -67,6 +67,7 @@ export default function (Vue, { router, head, isClient }) {
 				i++;
 			}
 			v = Math.round(v * 10) / 10;
+			if (ns) v = v.toFixed(1)
 			var vs = (v + '').replace(/\./g, ',').replace(/\d(?=(?:\d{3})+(?:,|$))/g, function ($0, i) { return $0 + ' ' });
 			return (neg ? '-' : '') + (vs + ' ' + suffixes[i] + ' Ft').trim();
 		}
