1cd0a7831e8a61990a683607ce0eb67ea0ea17bb
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-06 22:56:03 +0100

    fixed year color theme generation (using years from data)

--- scripts/prepare-style.js
+++ scripts/prepare-style.js
@@ -2,17 +2,19 @@ const fs = require('fs');
 const slugify = require('slugify');
 
 const config = JSON.parse(fs.readFileSync('src/data/config.json', { encoding: 'utf8' }));
-const { defaultYear, theme } = config;
+const data = JSON.parse(fs.readFileSync('src/data/data.json', { encoding: 'utf8' }));
 
-// theme: { year: color, ... }
+const { defaultYear, theme } = config; // theme: { year: color, ... }
+const defaultColor = theme[defaultYear] || '#0d6efd'; // fallback to BS $primary
+const years = Object.keys(data);
 
-const mapEntries = Object.keys(theme).map(y => {
+const mapEntries = years.map(y => {
 	const slugifiedYear = slugify(y);
-	const color = theme[y];
+	const color = theme[y] || defaultColor;
 	return `"${slugifiedYear}":${color}`
 }).join(',\n');
 
 const map = `$year-colors:(\n${mapEntries}\n);`;
-const $primary = theme[defaultYear] ? `$primary: ${theme[defaultYear]};` : '';
+const $primary = theme[defaultYear] ? `$primary: ${defaultColor};` : '';
 
 fs.writeFileSync('src/scss/_generated.scss', `${map}\n${$primary}`);
