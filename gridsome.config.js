1a79c95be21b35d656466d9355d6c9cd645fe37e
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-06 10:12:11 +0100

    added configurable GA code (ga.id)

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,20 @@
 const config = require('./src/data/config.json');
 
+function ga() {
+	if (config.ga && config.ga.id) {
+		return [{
+			use: '@gridsome/plugin-google-analytics',
+			options: {
+				id: config.ga.id
+			}
+		}]
+	}
+	return []
+}
+console.log(JSON.stringify(ga()));
 module.exports = {
-	plugins: [],
+	plugins: [
+		...ga(),
+	],
 	siteName: config.seo.siteName
 }
