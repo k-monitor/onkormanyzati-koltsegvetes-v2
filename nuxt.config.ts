d0e88632e160e2518063290917684d1da74af295
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-11-11 10:39:43 +0100

    silenced sass warnings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	modules: ['@nuxt/eslint'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					// TODO LATER upgrade Bootstrap to v5 and remove this
+					silenceDeprecations: ['abs-percent'],
+				},
+			},
+		},
+	},
 });
