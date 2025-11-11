3effef871d92b29187bc37408319db014f8f900f
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-11-11 16:35:45 +0100

    deduped head, added favicon link

--- app/components/DefaultLayout.vue
+++ app/components/DefaultLayout.vue
@@ -1,68 +1,6 @@
 <script setup lang="ts">
 const { canShowMilestones, year } = useYear();
 
-useHead({
-	bodyAttrs: {
-		id: 'page-top',
-	},
-	htmlAttrs: {
-		lang: 'hu',
-	},
-	link: [
-		{
-			rel: 'canonical',
-			href: CONFIG.url,
-		},
-		{
-			rel: 'stylesheet',
-			href: 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&amp;subset=latin-ext',
-		},
-		{
-			rel: 'stylesheet',
-			href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
-		},
-		{
-			rel: 'stylesheet',
-			href: 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css',
-		},
-	],
-	meta: [
-		{
-			property: 'og:site_name',
-			content: CONFIG.seo.siteName,
-		},
-		{
-			property: 'og:title',
-			content: CONFIG.seo.ogTitle,
-		},
-		{
-			name: 'description',
-			property: 'og:description',
-			content: CONFIG.seo.description,
-		},
-		{
-			property: 'og:url',
-			content: CONFIG.url,
-		},
-		{
-			property: 'og:type',
-			content: 'website',
-		},
-		{
-			property: 'og:image',
-			content: CONFIG.url + 'assets/img/ogimage.jpg',
-		},
-	],
-	script: [
-		{ src: 'https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js' },
-		{ src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js' },
-		{ src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js' },
-		{ src: 'https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.4.1/tinycolor.min.js' },
-		{ src: 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/intro.min.js' },
-	],
-	title: CONFIG.seo.pageTitle,
-});
-
 onMounted(() => {
 	// TODO LATER eliminate jQuery (might need Bootstrap-Vue)
 	const $ = window.$;
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,34 @@
+<script setup lang="ts">
+useHead({
+	link: [
+		{
+			rel: 'canonical',
+			href: CONFIG.url,
+		},
+
+		{
+			rel: 'stylesheet',
+			href: 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/introjs.min.css',
+		},
+	],
+	meta: [
+		{
+			property: 'og:title',
+			content: CONFIG.seo.ogTitle,
+		},
+		{
+			property: 'og:url',
+			content: CONFIG.url,
+		},
+	],
+	script: [
+		{ src: 'https://cdnjs.cloudflare.com/ajax/libs/tinycolor/1.4.1/tinycolor.min.js' },
+		{ src: 'https://cdnjs.cloudflare.com/ajax/libs/intro.js/2.9.3/intro.min.js' },
+	],
+	title: CONFIG.seo.pageTitle,
+});
+</script>
+
 <template>
 	<DefaultLayout />
 </template>
