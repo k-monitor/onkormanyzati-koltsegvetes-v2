d0a9213388141acff8632bd13cd2ee096656a958
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-11-17 10:44:15 +0100

    added favicon migrator script

--- scripts/prepare-downloads.js
+++ scripts/prepare-downloads.js
@@ -1,2 +1,14 @@
 import fs from 'fs';
 fs.copyFileSync('./input/budget.xlsx', './static/budget.xlsx');
+
+const newFavicon = './static/assets/img/favicon.png';
+const oldFavicon = './static/favicon.png';
+if (fs.existsSync(oldFavicon)) {
+	if (fs.existsSync(newFavicon)) {
+		console.error(
+			'[KÖKÖ] static/favicon.png és static/assets/img/favicon.png is létezik, de csak az utóbbit használja a program. Kérlek válaszd ki a megfelelőt, és azt tartsd meg static/assets/img/favicon.png útvonalon, a static/favicon.png-t pedig töröld!',
+		);
+	} else {
+		fs.renameSync(oldFavicon, newFavicon);
+	}
+}
