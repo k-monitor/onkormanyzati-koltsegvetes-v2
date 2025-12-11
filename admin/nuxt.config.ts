f28141da4bbe9aacc420e2f16f030329665cc45a
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2025-12-11 16:21:50 +0100

    added basic auth

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,5 +1,13 @@
+const { ADMIN_USER, ADMIN_PASS, SECOND_USER, SECOND_PASS } = process.env;
+const users = [{ username: ADMIN_USER || 'admin', password: ADMIN_PASS || 'admin' }];
+if (SECOND_USER && SECOND_PASS) {
+	users.push({ username: SECOND_USER, password: SECOND_PASS });
+}
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	modules: ['@kgierke/nuxt-basic-auth'],
+	basicAuth: { users },
 });
