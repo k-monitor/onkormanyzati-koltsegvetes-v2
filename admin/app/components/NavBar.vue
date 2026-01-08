5c56ac420f099188d398cc74bb13f71cb26b5dd1
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-01-08 15:55:44 +0100

    added docs link to admin navbar

--- admin/app/components/NavBar.vue
+++ admin/app/components/NavBar.vue
@@ -5,16 +5,25 @@ const links = [
 	{ href: '/logos/', text: 'Logók' },
 	{ href: '/milestones/', text: 'Fejlesztéskártyák képei' },
 	{ href: '/site/', text: 'Weboldal' },
+	{
+		href: 'https://github.com/k-monitor/onkormanyzati-koltsegvetes-v2',
+		text: 'Dokumentáció',
+		external: true,
+	},
 ];
 </script>
 <template>
 	<div>
-		<strong>KÖKÖ Admin | </strong>
+		<strong>KÖKÖ Admin |</strong>
 		<template
 			v-for="(link, index) in links"
 			:key="link.href"
 		>
-			<NuxtLink :to="link.href">{{ link.text }}</NuxtLink>
+			<NuxtLink
+				:to="link.href"
+				:target="link.external ? '_blank' : undefined"
+				>{{ link.text }}</NuxtLink
+			>
 			<span v-if="index < links.length - 1"> | </span>
 		</template>
 	</div>
