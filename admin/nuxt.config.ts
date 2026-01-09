24d3882598c90ae9520d056ea6dcfcdce498702e
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-01-09 15:40:30 +0100

    added shadcn

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,7 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
+	css: ['~/assets/css/tailwind.css'],
 	devtools: { enabled: true },
+	modules: ['@vueuse/nuxt', 'shadcn-nuxt'],
 	runtimeConfig: {
 		kokoDir: '', // NUXT_KOKO_DIR
 		admin: {
@@ -16,5 +20,12 @@ export default defineNuxtConfig({
 		public: {
 			url: '', // NUXT_PUBLIC_URL
 		},
+	},
+	shadcn: {
+		componentDir: '@/components/ui',
+		prefix: '',
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
