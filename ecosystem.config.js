bf826d94f7759be59765a94f7cb0ae91ec334bc9
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-02-20 16:06:59 +0100

    added interpreter to pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: 'koko-admin',
+			interpreter: 'node@24.12.0',
 			node_args: '--env-file=admin/.env',
 			script: 'admin/.output/server/index.mjs',
 			log_date_format: 'YYYY-MM-DD HH:mm Z',
