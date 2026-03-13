b8c3a590f9bad9bc253604207c9fd07a066514ff
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-03-13 16:09:29 +0100

    fixed handling of "ebből:" rows in budget editor

--- admin/app/components/BudgetEditorNode.vue
+++ admin/app/components/BudgetEditorNode.vue
@@ -20,46 +20,68 @@ const sum = computed(() => {
 	return node.children.reduce((acc, child) => acc + (child.value || 0), 0);
 });
 
+const areChildrenIncomplete = computed(() => {
+	if (!node.children) return false;
+	return node.children.some((child) => child.name.startsWith('ebből:'));
+});
+
+const showChildrenSumWarning = computed(() => {
+	if (areChildrenIncomplete.value) {
+		return node.value < (sum.value || 0);
+	}
+	return node.value !== sum.value;
+});
+
 const open = ref(false);
 
 const sheet = inject<Ref<Worksheet | undefined>>('sheet');
 
-function findEconRow(nodeId: string | number) {
+function findEconRow(id: string | number, name: string) {
 	if (!sheet?.value) return null;
 	for (let ri = 3; ri <= sheet.value.rowCount; ri++) {
 		// index is 1-based, header is at least 2 rows
 		const row = sheet.value.getRow(ri);
-		const cellValue = row.getCell(2).value?.toString() || '';
-		const needle = `(${nodeId})`;
-		if (cellValue.includes(needle)) {
-			return row;
+		const cellValue = (row.getCell(2).value?.toString() || '').trim();
+		if (name.startsWith('ebből:')) {
+			// "ebből:" rows have IDs of their parent in the input
+			// but we have "ID:index" ID for them after preparation
+			const parentId = String(id).split(':')[0];
+			const needle = `(${parentId})`;
+			if (cellValue.startsWith(name) && cellValue.endsWith(needle)) {
+				return row;
+			}
+		} else {
+			const needle = `(${id})`;
+			if (cellValue.endsWith(needle)) {
+				return row;
+			}
 		}
 	}
 	return null;
 }
 
-function readEconValue(nodeId: string | number) {
-	const row = findEconRow(nodeId);
+function readEconValue(id: string | number, name: string) {
+	const row = findEconRow(id, name);
 	if (!row) return undefined;
 	const valueCell = row.getCell(3);
 	const rawValue = (valueCell.result || valueCell.value)?.toString() || '';
 	return Number(rawValue.replace(/[^0-9-]+/g, ''));
 }
 
-function writeEconValue(nodeId: string | number, value: number) {
-	const row = findEconRow(nodeId);
+function writeEconValue(id: string | number, name: string, value: number) {
+	const row = findEconRow(id, name);
 	if (!row) return;
 	const valueCell = row.getCell(3);
 	valueCell.value = value;
 }
 
-const inputValue = ref(readEconValue(node.id || ''));
+const inputValue = ref(readEconValue(node.id || '', node.name || ''));
 const bus = useEventBus(CELL_CHANGED_EVENT);
 watchThrottled(
 	inputValue,
 	() => {
 		if (inputValue.value === undefined) return;
-		writeEconValue(node.id || '', inputValue.value);
+		writeEconValue(node.id || '', node.name || '', inputValue.value);
 		bus.emit();
 	},
 	{
@@ -118,7 +140,7 @@ watchThrottled(
 					<div v-else>{{ Number(node.value).toLocaleString('hu') }}</div>
 				</div>
 				<div
-					v-if="node.value !== sum"
+					v-if="showChildrenSumWarning"
 					:class="cn('text-destructive flex items-center gap-3', isEditable && 'pr-11')"
 				>
 					<Sigma class="size-4" />
