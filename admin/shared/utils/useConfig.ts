8ff4b60b3c594822c7003cd144dd319b666c3caf
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2026-03-19 10:49:45 +0100

    we don't actually need NUXT_ prefixed variations

--- admin/shared/utils/useConfig.ts
+++ admin/shared/utils/useConfig.ts
@@ -10,7 +10,5 @@ export default () => ({
 
 function get(key: string, defaultValue: string = ''): string {
 	if (!import.meta.server) return ''; // make sure read can only happen on server
-	const NUXT_KEY = process.env[`NUXT_${key}`];
-	const KEY = process.env[key];
-	return NUXT_KEY || KEY || defaultValue;
+	return process.env[key] || defaultValue;
 }
